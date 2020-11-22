a42d6a49baa20e3d5cc85fe2222030da69ac0125
Muhammad Tayyab <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2020-11-22 11:49:47 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue]= useState({});
+
+  useEffect(() => {
+    fetch( `/.netlify/functions/helloworld?name=from Serverless Function`)
+    .then(res=> res.json())
+    .then(obj=>setValue(obj))
+    return () => {
+    
+    }
+  }, [])
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>{value.message}</h1>
     </div>
   );
 }
